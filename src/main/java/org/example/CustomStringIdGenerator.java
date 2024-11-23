f1c750e73eec2e87a7de8a4d94b550b84868ae91
snowmonthtree <2680340431@qq.com>
2024-11-23 22:38:49 +0800

    1

--- src/main/java/org/example/CustomStringIdGenerator.java
+++ src/main/java/org/example/CustomStringIdGenerator.java
@@ -1,17 +1,29 @@
 package org.example;
+import org.example.enity.User;
+import org.example.repository.UserRepository;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
 import java.io.Serializable;
+import java.util.Random;
 import java.util.UUID;
 
 public class CustomStringIdGenerator implements IdentifierGenerator {
+    private UserRepository userRepository;
 
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
-        UUID uuid = UUID.randomUUID();
-        String uuidStr = uuid.toString().replaceAll("-", "");
-        return uuidStr.substring(0, Math.min(uuidStr.length(), 10));
+        Random random=new Random();
+        int uuid = random.nextInt(100000000+1);
+        String uuidStr =Integer.toString(uuid);
+        User user;
+        user=userRepository.findByUserId(uuidStr);
+        while( user!=null){
+            uuid = random.nextInt(100000000+1);
+            uuidStr =Integer.toString(uuid);
+            user=userRepository.findByUserId(uuidStr);
+        }
+        return uuidStr;
     }
 }
--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
     // 可以添加自定义查询方法
     User findByUserIdAndPassword(String userId, String password);
+    User findByUserId(String userId);
 }
