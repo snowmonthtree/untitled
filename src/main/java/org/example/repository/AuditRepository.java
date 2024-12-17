71435072c55102bc3e16ba2223584da4c5024748
945U945 <1069718692@qq.com>
2024-12-17 22:58:37 +0800

    上传记录

---
+++ src/main/java/org/example/repository/AuditRepository.java
@@ -0,0 +1,14 @@
+package org.example.repository;
+
+import org.example.enity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuditRepository extends JpaRepository<Audit, String> {
+    List<Audit> OrderByAuditTimeDesc();
+
+    Audit findByAuditId(String auditId);
+}
