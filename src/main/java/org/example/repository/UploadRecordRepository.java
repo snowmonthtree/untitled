d3d8094a0466924ac0c81ba0d0a13c1829526363
945U945 <1069718692@qq.com>
2024-12-17 22:43:54 +0800

    上传记录

--- src/main/java/org/example/repository/UploadRecordRepository.java
+++ src/main/java/org/example/repository/UploadRecordRepository.java
@@ -2,11 +2,15 @@ package org.example.repository;
 
 import org.example.enity.UploadRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UploadRecordRepository extends JpaRepository<UploadRecord, String> {
+    @Query("SELECT ur FROM UploadRecord ur WHERE ur.user.userId = :userId ORDER BY ur.uploadTime DESC")
     List<UploadRecord> findByUser_UserId(String userId);
+
+    List<UploadRecord> findByResource_ResourceId(String resourceId);
 }
